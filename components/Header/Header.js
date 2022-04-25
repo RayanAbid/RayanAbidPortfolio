68e35db6f15e4030eedf398b7a095ea418bd3143
Rayan Abid <68749736+RayanAbid@users.noreply.github.com>
2022-04-25 14:15:48 +0500

    Update Header.js

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -11,7 +11,7 @@ export default function Navbar({ fixed }) {
               className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
               href="#pablo"
             >
-              main Starter Kit
+Rayan Abid
             </a>
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
@@ -28,35 +28,35 @@ export default function Navbar({ fixed }) {
             }
             id="example-navbar-danger"
           >
-            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-              <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#pablo"
-                >
-                  <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i>
-                  <span className="ml-2">Share</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#pablo"
-                >
-                  <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i>
-                  <span className="ml-2">Tweet</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#pablo"
-                >
-                  <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i>
-                  <span className="ml-2">Pin</span>
-                </a>
-              </li>
-            </ul>
+//             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+//               <li className="nav-item">
+//                 <a
+//                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+//                   href="#pablo"
+//                 >
+//                   <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i>
+//                   <span className="ml-2">Share</span>
+//                 </a>
+//               </li>
+//               <li className="nav-item">
+//                 <a
+//                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+//                   href="#pablo"
+//                 >
+//                   <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i>
+//                   <span className="ml-2">Tweet</span>
+//                 </a>
+//               </li>
+//               <li className="nav-item">
+//                 <a
+//                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+//                   href="#pablo"
+//                 >
+//                   <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i>
+//                   <span className="ml-2">Pin</span>
+//                 </a>
+//               </li>
+//             </ul>
           </div>
         </div>
       </nav>
