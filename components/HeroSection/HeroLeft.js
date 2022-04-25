62a423e42af1f9c5901d7abdf9cee597d042a51e
Rayan Abid <68749736+RayanAbid@users.noreply.github.com>
2022-04-25 14:20:11 +0500

    Update HeroLeft.js

--- components/HeroSection/HeroLeft.js
+++ components/HeroSection/HeroLeft.js
@@ -12,7 +12,7 @@ function HeroLeft() {
           </span>
         </h1>
         <h2 className="md:w-8/12 py-4 text-center md:text-left md:py-8 text-dark text-lg lg:text-2xl">
-            Experienced Frontend Web Developer and Applications Developer with a demonstrated history of working in the Development industry. Skilled in React Js, React Native, Firebase, Node Js, Strong engineering professional with a intermediate focused in Computer Science from Punjab College Lahore and currently focusing in Bachelors of Computer Science in Virtual University 
+            Experienced Frontend Web Developer and Applications Developer with a demonstrated history of working in the Development industry. Skilled in React Js, React Native, Firebase, Node Js.
         </h2>
         <div className="w-full flex justify-center md:block">
           <button className="hover:opacity-90 bg-white py-3 px-10 lg:py-7 lg:px-20 rounded-full text-main text-sm md:text-lg f-f-p">
